389782f550719bf397894109a71096d94d9bee46
Yohaan <yohanakipruto@gmail.com>
2025-12-19 22:01:59 +0300

    Initial Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-      <section className='bg-fuchsia-950 mt-10 items-center'>
-              <h2 className='text-white font-serif text-3xl text-center my-8'>Loan Calculator</h2>
-              <p className='text-white font-serif text-center'>Calculate your monthly payment and total loan cost</p>
+      <section className='bg-fuchsia-950 mt-10 mx-auto'> 
+      <div className='flex flex-col justify-center items-center py-10 px-4 md:px-0'>
+        <h2 className='text-white font-serif text-3xl my-8 text-center'>Loan Calculator</h2>
+              <p className='text-white font-serif text-center'>Calculate your monthly payment and <span className='block md:inline-block space-y-3'>total loan cost</span></p>
+      </div>
     </section>
   )
 }
