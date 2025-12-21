d36bb7572ef7c723d3e609236a49ec519df6e5b5
Yohaan <yohanakipruto@gmail.com>
2025-12-21 11:50:29 +0300

    Initial Commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,9 @@ import { FaChevronDown, FaExclamationTriangle } from 'react-icons/fa';
 const Card = () => {
   return (
       <section className='bg-fuchsia-950 py-10 max-w-7xl mx-4 my-2 md:mx-auto'>
-          <div className='grid sm:grid-rows-1 md:grid-cols-2 gap-3 space-y-3 md:space-y-0 items-center justify-center md:mx-8 md:justify-evenly'>
-              <div className='w-96 bg-white/10 rounded-2xl backdrop-blur-xl p-4 overflow-hidden'>
+          <div className='container max-w-6xl mx-auto py-6 px-4'>
+              <div className='grid sm:grid-rows-1 md:grid-cols-2 gap-3 space-y-3 md:space-y-0 items-center justify-center md:justify-between'>
+                  <div className='bg-white/10 rounded-2xl backdrop-blur-xl p-4 overflow-hidden'>
                   <h1 className='text-white font-serif p-2 border-b-2 border-fuchsia-700'>Loan Details</h1>
                   <div className='mt-4'>
                       <label className='text-white font-serif'>Loan Amount</label>
@@ -20,26 +21,26 @@ const Card = () => {
 
                           <div className='block mt-3 md:mt-0'>
                               <h1 className='text-white font-serif'>Loan Terms</h1>
-                              <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 text-white bg-white/10 hover:outline-none outline-none'>
-                                  <input type="text" className='hover:outline-none outline-none' placeholder='Select' />
-                                  <FaChevronDown className='text-gray-700'/>
+                              <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 text-white bg-white/10'>
+                                  <p className='font-serif'>Select</p>
+                                  <FaChevronDown className='text-gray-700 '/>
                               </div>
                           </div>
                       </div>
                       <div className='grid grid-rows-1 md:grid-cols-2 items-stretch gap-4 my-3'>
                           <div>
                               <h1 className='text-white font-serif'>Start date</h1>
-                          <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 text-white bg-white/10 hover:outline-none outline-none'>
-                                  <input type="text" className='hover:outline-none outline-none' placeholder='Select' />
-                                  <FaChevronDown className='text-gray-700'/>
+                          <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 text-white bg-white/10'>
+                                  <p className='font-serif'>Select</p>
+                                  <FaChevronDown className='text-gray-700 '/>
                               </div>
                           </div>
 
                           <div>
                               <h1 className='text-white font-serif'>Start date</h1>
-                              <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 text-white bg-white/10 hover:outline-none outline-none'>
-                                  <input type="text" className='hover:outline-none outline-none' placeholder='Select' />
-                                  <FaChevronDown className='text-gray-700'/>
+                              <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 text-white bg-white/10'>
+                                  <p className='font-serif'>Select</p>
+                                  <FaChevronDown className='text-gray-700 '/>
                               </div>
                           </div>
                       </div>
@@ -47,7 +48,7 @@ const Card = () => {
                   <button className='my-2 px-4 py-1 p-2 rounded-md bg-cyan-500 text-black font-serif w-full hover:scale-110 hover:translate-y-1 transition-transform duration-300'>Calculate</button>
               </div>
           
-              <div className='w-96 max-w-sm bg-white/30 rounded-2xl backdrop-blur-xl p-4 overflow-hidden'>
+              <div className='h-full bg-white/30 rounded-2xl backdrop-blur-xl p-4 overflow-hidden'>
                   <h1 className='text-white font-serif p-2 border-b-2 border-cyan-400'>Result</h1>
                   <div className='mt-4'>
                       <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 bg-white my-3'>
@@ -66,12 +67,13 @@ const Card = () => {
                           <h1 className='text-white font-serif'>Loan payoff progress</h1>
                           <p className='text-white font-semibold'>55%</p>
                       </div>
-                      <p className='rounded-sm w-full px-2 py-1 my-3 bg-linear-to-r from-blue-600/50 to-white/50'></p>
+                      <p className='rounded-sm w-full px-2 py-1 my-3 bg-gradient-to-r from-blue-600/50 to-white/50'></p>
                       <div className='flex flex-row items-center space-x-3 justify-between w-full rounded-md px-4 py-3 bg-white my-3'>
                                   <FaExclamationTriangle size={20} color="black" />
                                   <p className='text-bold'>The results are estimates.Actual payments may vary</p>
                       </div>
                   </div>
+              </div>
               </div>
           </div>
     </section>
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,9 +3,10 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 
 const Results = () => {
   return (
-      <section className='bg-fuchsia-950 max-w-7xl mx-4 my-2 md:mx-auto'>
-          <div className='grid sm:grid-rows-1 md:grid-cols-3 gap-3 items-stretch justify-center md:justify-evenly'>
-              <div className='w-96 p-4 rounded-md shadow-2xl backdrop-blur-3xl bg-fuchsia-600/30'>
+      <section className='bg-fuchsia-950 py-10'>
+          <div className='container max-w-6xl mx-auto py-6 px-4'>
+              <div className='grid sm:grid-rows-1 md:grid-cols-3 gap-3 items-center justify-center md:justify-between'>
+                  <div className='p-4 rounded-md shadow-2xl backdrop-blur-3xl bg-fuchsia-600/30'>
                   <h1 className='text-white text-lg font-semibold'>Payment Schedule</h1>
                   <p className='text-white my-2'>View detailed monthly breakdown</p>
                   <div className='flex flex-row items-center'>
@@ -13,7 +14,7 @@ const Results = () => {
                       <FaLongArrowAltRight className='text-cyan-600 ml-2'/>
                   </div>
               </div>
-              <div className='w-96 p-4 rounded-md shadow-2xl backdrop-blur-3xl bg-fuchsia-600/30'>
+              <div className='p-4 rounded-md shadow-2xl backdrop-blur-3xl bg-fuchsia-600/30'>
                   <h1 className='text-white text-lg font-semibold'>Compare Loan</h1>
                   <p className='text-white my-2'>View detailed monthly breakdown</p>
                   <div className='flex flex-row items-center'>
@@ -21,13 +22,14 @@ const Results = () => {
                       <FaLongArrowAltRight className='text-cyan-600 ml-2'/>
                   </div>
               </div>
-              <div className='w-96 p-4 rounded-md shadow-2xl backdrop-blur-3xl bg-fuchsia-600/30'>
+              <div className='p-4 rounded-md shadow-2xl backdrop-blur-3xl bg-fuchsia-600/30'>
                   <h1 className='text-white text-lg font-semibold'>Save Result</h1>
                   <p className='text-white my-2'>Export or save you calculation</p>
                   <div className='flex flex-row items-center'>
                       <p className='text-cyan-600 font-semibold'>Export</p>
                       <FaLongArrowAltRight className='text-cyan-600 ml-2'/>
                   </div>
+              </div>
               </div>
           </div>
     </section>
